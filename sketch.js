f275685e2d155d3eac473c94105897c09fa2c476
Danfigotin <43012387+Danfigotin@users.noreply.github.com>
2018-11-30 13:25:31 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,19 +2,19 @@
 // https://www.kadenze.com/courses/the-nature-of-code
 // http://natureofcode.com/
 // Session 2: Array of Particles, multiple forces
-
 var person;
-//var obstacle;
-
 function setup() {
-  createCanvas(640, 360);
-
-	Person = new Person();
+  createCanvas(640, 640);
+  person = new Person();
+  
 }
-
 
 function draw() {
   background(51);
- Person.update();
-	Person.display();
-}
+ person.update();
+ person.display();
+  
+  
+  fill(124,124,124,124);
+  rect(400,height-50,50,50);
+  }
